81c02cec0612c126f7dba5b1d0a4d7028b477146
Simon <simonj40@gmail.com>
2016-03-04 15:29:54 +0100

    Fixed order info display all the way

--- assets/js/estimator.js
+++ assets/js/estimator.js
@@ -29,6 +29,10 @@ function estimator() {
                 $('#estimate').show();
                 $('#shipping-estimator').css({'opacity': 1});
                 $('#rate-estimate').html('$' + method_data.result.rate);
+                setRateEstimatedShipping(method_data.result.rate);
+                var total_before_tax = parseFloat(cart_data.result.subtotal) + parseFloat(method_data.result.rate);
+                var total_before_tax = total_before_tax.toFixed(2).toLocaleString();
+                setTotalBeforeTax(total_before_tax);
                 if (method_data.result.date_range.length > 0){
                     $('#block-date-estimate').show();
                     $('#date-estimate').html(method_data.result.date_range[0] + ' - ' + method_data.result.date_range[1]);
@@ -36,6 +40,7 @@ function estimator() {
                     $('#block-date-estimate').hide();
                 }
                 $('#tax-estimate').html('$' + tax_data.result.tax);
+                setTaxEstimated(tax_data.result.tax);
                 var total = parseFloat(cart_data.result.subtotal) + parseFloat(tax_data.result.tax) + parseFloat(method_data.result.rate);
                 $('#total-estimate').html('$' + total.toFixed(2).toLocaleString());
             });
@@ -58,6 +63,32 @@ $('#cart_zip_code').keypress(function(e) {
 
 $('#cart_ClearEstimate').click(function(e) {
     e.preventDefault();
+    clearEstimated();
     $('#estimate').hide();
     $('#form').show();
 });
+function setRateEstimatedShipping(rate){
+    $('#rate-estimate-checkout').find('.amount').text('$'+rate);
+    $('#rate-estimate-checkout').show();
+}
+function setTaxEstimated(tax){
+    $('#tax-estimate-checkout').find('.amount').text('$'+tax);
+    $('#tax-estimate-checkout').show();
+}
+function setTotalEstimated(total){
+    var estimate_total = $('#estimate-total').find('.amount');
+    estimate_total.data("old-value", estimate_total.text());
+    estimate_total.text('$'+total);
+}
+function setTotalBeforeTax(total){
+    $('#total-before-tax').find('.amount').text('$'+total);
+    $('#total-before-tax').show();
+}
+function clearEstimated(){
+    $('#rate-estimate-checkout').hide();
+    $('#tax-estimate-checkout').hide();
+    $('#total-before-tax').hide();
+    //Restore total
+    var estimate_total = $('#estimate-total').find('.amount');
+    estimate_total.text( estimate_total.data("old-value") );
+}
